3e5c7240f4d0cb09315f7826dfabe5a7a0a0b60b
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-07-28 02:50:31 -0300

    Separação das responsabilidades na atribuição da informação de como o attributo será encontrado

--- src/main/java/br/com/rjconsultores/tests/webmodule/seleniumcore/events/FindBy.java
+++ src/main/java/br/com/rjconsultores/tests/webmodule/seleniumcore/events/FindBy.java
@@ -1,19 +1,21 @@
 package br.com.rjconsultores.tests.webmodule.seleniumcore.events;
 
+import br.com.rjconsultores.tests.webmodule.seleniumcore.enums.IdentifyBy;
+
 public class FindBy {
-	private String id;
+	private IdentifyBy identifyBy;
 	private String value;
 	
-	public FindBy(String id, String value) {
-		this.id = id;
+	public FindBy(IdentifyBy identifyBy, String value) {
 		this.value = value;
-	}
-	
-	public String getId() {
-		return id;
+		this.identifyBy = identifyBy;
 	}
 	
 	public String getValue() {
 		return value;
+	}
+	
+	public IdentifyBy getIdentifyBy() {
+		return identifyBy;
 	}
 }
